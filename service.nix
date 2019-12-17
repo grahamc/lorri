1b1af7056a9c74164bae2e70c94b7bdc5a108fcd
Graham Christensen <graham@grahamc.com>
2019-12-17 12:54:17 -0500

    service.nix: clean up, delete useless services

--- service.nix
+++ service.nix
@@ -3,17 +3,9 @@ let
   lorri = import ./default.nix {};
   lorriBin = if true then ./target/debug/lorri else "${lorri}/bin/lorri";
 
-   script = pkgs.writeShellScript "lol" ''
-     started=$(date '+%s')
-     while sleep 5; do
-       printf "This service has been up %s seconds\n" $(($(date '+%s') - started))
-     done
-   '';
-
-   check = pkgs.writeShellScript "check" ''
+  check = pkgs.writeShellScript "check" ''
      while sleep 5; do
        cargo fmt && cargo check && cargo clippy && cargo build
-       printf "This service has been up %s seconds\n" $(($(date '+%s') - started))
      done
    '';
 
@@ -25,11 +17,6 @@ pkgs.writeText "services.json" (
         name = "lorri";
         program = lorriBin;
         args = [ "daemon" ];
-      }
-      {
-        name = "the-time-is";
-        program = script;
-        args = [];
       }
       {
         name = "check";
